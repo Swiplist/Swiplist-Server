0bd7d3440dc7a22dba13850f7f04a6746d722de3
Energy0124 <energystar.0124@gmail.com>
2018-05-10 18:10:02 +0800

    add description to user model

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -25,6 +25,9 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  description: {
+    type: String
+  },
   friends: [{ type: String }],
   items: [{ type: String }],
   iconUrl: {
