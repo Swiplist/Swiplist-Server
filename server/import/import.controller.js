349834b0d87fecf34cea0682bb2e10c1c8bf2f93
Energy0124 <energystar.0124@gmail.com>
2018-05-12 11:34:56 +0800

    Fix eslintrc to remove no-param-reassign

--- server/import/import.controller.js
+++ server/import/import.controller.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
-const httpStatus = require('http-status');
-const APIError = require('../helpers/APIError');
+// const jwt = require('jsonwebtoken');
+// const httpStatus = require('http-status');
+// const APIError = require('../helpers/APIError');
 const Item = require('../item/item.model');
 const User = require('../user/user.model');
 const config = require('../../config/config');
-const steam = require('steam-web');
+const Steam = require('steam-web');
 
 // const logger = require('../../config/winston');
 
@@ -17,7 +17,7 @@ const steam = require('steam-web');
  * @returns {*}
  */
 function malAnime(req, res, next) {
-
+  return next();
 }
 
 /**
@@ -28,7 +28,7 @@ function malAnime(req, res, next) {
  * @returns {*}
  */
 function malManga(req, res, next) {
-
+  return next();
 }
 
 /**
@@ -39,40 +39,42 @@ function malManga(req, res, next) {
  * @returns {*}
  */
 function steamGames(req, res, next) {
-  const s = new steam({
+  const s = new Steam({
     apiKey: config.steamWebApiKey,
-    format: 'json' //optional ['json', 'xml', 'vdf']
+    format: 'json' // optional ['json', 'xml', 'vdf']
   });
-  s.resolveVanityURL({
-    vanityurl: 'energy0124',
-    callback: function (err, data) {
-      console.log(data);
-    }
-  });
+  // s.resolveVanityURL({
+  //   vanityurl: 'energy0124',
+  //   callback: function (err, data) {
+  //     console.log(data);
+  //   }
+  // });
   return s.getOwnedGames({
     steamid: req.body.steamId,
     include_appinfo: 1,
     include_played_free_games: 1,
-    callback: function (err, data) {
-      let games = data.response.games;
-      let gameIds = games.map(game => game.appid);
-      let gameItems = [];
+    callback(err, data) {
+      const games = data.response.games;
+      const gameIds = games.map(game => game.appid);
+      const gameItems = [];
       return Item.find({
         'metadata.appid': {
           $in: gameIds
         }
       })
         .exec()
-        .then(items => {
+        .then((items) => {
           gameItems.push(...items);
-          let newGames = games.filter(game => !items.find(item => parseInt(game.appid) === item.metadata.appid));
-          let itemsToBeSaved = [];
-          for (let newGame of newGames) {
-            let item = new Item({
+          const newGames = games.filter(game => !items.find(
+            item => parseInt(game.appid, 10) === item.metadata.appid)
+          );
+          const itemsToBeSaved = [];
+          for (const newGame of newGames) {
+            const item = new Item({
               name: newGame.name,
               imageUrl: `http://media.steampowered.com/steamcommunity/public/images/apps/${newGame.appid}/${newGame.img_logo_url}.jpg`,
               metadata: {
-                //game
+                // game
                 appid: newGame.appid,
                 playtime_forever: newGame.playtime_forever,
                 img_icon_url: `http://media.steampowered.com/steamcommunity/public/images/apps/${newGame.appid}/${newGame.img_icon_url}.jpg`
@@ -83,33 +85,30 @@ function steamGames(req, res, next) {
           }
           return Item.create(itemsToBeSaved)
             .then(
-              savedItems => {
+              (savedItems) => {
                 if (savedItems) gameItems.push(...savedItems);
                 return User.get(req.user.id)
-                  .then(user => {
-                    let likedGames = user.games;
+                  .then((user) => {
+                    const likedGames = user.games;
                     likedGames.push(...gameItems);
-                    likedGames.sort((a, b) => b.metadata.playtime_forever - a.metadata.playtime_forever);
+                    likedGames.sort(
+                      (a, b) => b.metadata.playtime_forever - a.metadata.playtime_forever
+                    );
                     user.games = [...new Set(likedGames.map(game => String(game._id)))];
                     return user.save()
-                      .then(savedUser => {
-                        return savedUser.populate('games')
-                          .execPopulate()
-                          .then(populatedUser => {
-                            return res.json(populatedUser);
-                          })
-                          .catch(err => next(err));
-                      })
-                      .catch(err => next(err));
+                      .then(savedUser => savedUser.populate('games')
+                        .execPopulate()
+                        .then(populatedUser => res.json(populatedUser))
+                        .catch(e => next(e)))
+                      .catch(e => next(e));
                   })
-                  .catch(err => next(err));
+                  .catch(e => next(e));
               })
-            .catch(err => next(err));
+            .catch(e => next(e));
         })
-        .catch(err => next(err));
+        .catch(e => next(e));
     }
   });
-
 }
 
 
